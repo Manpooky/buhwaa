c18c4e1c158fc3096d4550d6138fbf1e032020da
Anthony Maniko <maniko@Anthonys-MacBook-Air.local>
2025-06-21 17:06:39 -0700

    Add Swagger API documentation

--- im-buddy/backend/urls.py
+++ im-buddy/backend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('apps.visa_info.urls')),
+    # API endpoints grouped by app
+    path('visa-info/', include('apps.visa_info.urls')),
     path('translations/', include('apps.translations.urls')),
     # path('tips/', include('apps.tips.urls')),  # Removed for now
 ] 
